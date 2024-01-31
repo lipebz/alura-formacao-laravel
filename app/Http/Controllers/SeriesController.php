f09ca9c306100ed6ffa52807e9ac859d9ceb62c7
Filipe Bezerra <felipe.vitorio.bz@gmail.com>
2024-01-31 08:19:09 -0300

    Criando componente simples de visualização com blade

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -14,12 +14,6 @@ class SeriesController extends Controller
             'Grey\'s Anatomy'
         ];
 
-        $html = '<ul>';
-        foreach ($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-        $html .= '</ul>';
-
-        return $html;
+        return view('pages.series.index', compact('series'));
     }
 }
